c777177af12e16d957c622a6e3e428426dfc2377
Jonathan A. McCormick, Jr. <67705789+JonathanMcCormickJr@users.noreply.github.com>
2024-11-30 21:04:23 -0600

    updating routes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 #[macro_use] extern crate rocket;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, Bartlesville!"
+fn map_root() -> &'static str {
+    "Map of Bartlesville recycling options"
 }
 
 #[get("/map")]
-fn map() _> &'static str {
+fn map() -> &'static str {
     "Map of Bartlesville recycling options"
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .mount("/", routes![index])
+        .mount("/", routes![map_root, map])
 }
