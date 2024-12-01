a5614b02c77117bc3c18d66ce68c562a80024cf5
Jonathan A. McCormick, Jr. <67705789+JonathanMcCormickJr@users.noreply.github.com>
2024-11-30 19:53:18 -0600

    Adding favicon

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use rocket::fs::{FileServer, relative};
+
+#[get("/favicon.ico")]
+fn favicon() -> Option<rocket::fs::NamedFile> {
+    rocket::fs::NamedFile::open("static/favicon.ico").ok()
+}
+
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, Bartlesville!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+    .mount("/", routes![index])
+    .mount("/static", FileServer::from(relative!("static")))
 }
