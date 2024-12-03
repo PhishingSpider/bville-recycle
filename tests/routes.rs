4afc9f0def7940c38984be6e3421b02aea555249
Jonathan A. McCormick, Jr. <67705789+JonathanMcCormickJr@users.noreply.github.com>
2024-12-03 08:12:26 -0600

    Create test for the about function

--- tests/routes.rs
+++ tests/routes.rs
@@ -2,7 +2,7 @@ use bville_recycle::rocket;
 use rocket::http::Status; // Import HTTP status for response checks
 use rocket::local::blocking::Client; // Import the blocking client for testing // Import the rocket function from the library
 
-#[test] // Tests the map_root function "/"
+#[test]  // Tests the map_root function "/"
 fn test_map_root() {
     let rocket = rocket();
     let client = Client::tracked(rocket).expect("valid rocket instance");
@@ -14,7 +14,7 @@ fn test_map_root() {
     );
 }
 
-#[test] // Tests the map function "map"
+#[test]  // Tests the map function "/map"
 fn test_map() {
     let rocket = rocket();
     let client = Client::tracked(rocket).expect("valid rocket instance");
@@ -24,4 +24,16 @@ fn test_map() {
         response.into_string(),
         Some("Map of Bartlesville recycling options".into())
     );
+}
+
+#[test]  // Tests the about function "/about"
+fn test_about() {
+    let rocket = rocket();
+    let client = Client::tracked(rocket).expect("valid rocket instance");
+    let response = client.get("/about").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(
+        response.into_string(),
+        Some("About Bville Recycle".into())
+    )
 }
